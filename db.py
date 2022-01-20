a78d34624afb04c0482655aacc893b8848cb7342
Dimbler <dimbler@gmail.com>
2022-01-20 09:31:55 +0300

    add heroku integration approach 4

---
+++ db.py
@@ -0,0 +1,15 @@
+import os
+
+from sqlmodel import create_engine, SQLModel, Session
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # or other relevant config var
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
+from typing import List
+from sqlalchemy import select
 from fastapi.responses import HTMLResponse, JSONResponse
 from models import Song, SongCreate
+from db import get_session
+from sqlmodel import Session
 
 app = FastAPI()
 
@@ -11,3 +15,9 @@ def read_root():
 @app.get("/ping", response_class=JSONResponse)
 async def pong():
     return {"ping": "pong!"}
+
+@app.get("/songs", response_model=List[Song])
+def get_songs(session: Session = Depends(get_session)):
+    result = session.execute(select(Song))
+    songs = result.scalars().all()
+    return [Song(name=song.name, artist=song.artist, id=song.id) for song in songs]
